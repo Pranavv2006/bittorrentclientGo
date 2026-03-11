1289ab46e32e9c708c0ef98019bbd6beef71a177
pranavv2006 <pranavvernekar023@gmail.com>
2026-03-11 18:24:24 +0000

    helper function for converting bencode to readerable format for client

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/pranavv2006/bittorrentclientGo
 
 go 1.25.7
+
+require github.com/jackpal/bencode-go v1.0.2
--- torrentfile/torrentfile.go
+++ torrentfile/torrentfile.go
@@ -1,28 +1,65 @@
 package torrentfile
 
 import (
-	"io"
+	"os"
 
 	"github.com/jackpal/bencode-go"
 )
 
+// TorrentFile encodes the metadata from a .torrent file
+type TorrentFile struct {
+	Announce    string
+	InfoHash    [20]byte
+	PieceHashes [][20]byte
+	PieceLength int
+	Length      int
+	Name        string
+}
+
 type bencodeInfo struct {
-	Peices      string `bencode:"pieces"`
-	PeiceLength int    `bencode:"piece length"`
+	Pieces      string `bencode:"pieces"`
+	PieceLength int    `bencode:"piece length"`
 	Length      int    `bencode:"length"`
 	Name        string `bencode:"name"`
 }
 
 type bencodeTorrent struct {
-	Announce string      `bencode: "announce"`
+	Announce string      `bencode:"announce"`
 	Info     bencodeInfo `bencode:"info"`
 }
 
-func Open(r io.Reader) (*bencodeTorrent, error) {
-	bto := bencodeTorrent{}
-	err := bencode.Unmarshal(r, &bto)
+// Open parses a torrent file
+func Open(path string) (TorrentFile, error) {
+	file, err := os.Open(path)
 	if err != nil {
-		return nil, err
+		return TorrentFile{}, err
 	}
-	return &bto, nil
+	defer file.Close()
+
+	bto := bencodeTorrent{}
+	err = bencode.Unmarshal(file, &bto)
+	if err != nil {
+		return TorrentFile{}, err
+	}
+	return bto.toTorrentFile()
+}
+
+func (bto *bencodeTorrent) toTorrentFile() (TorrentFile, error) {
+	infoHash, err := bto.Info.hash()
+	if err != nil {
+		return TorrentFile{}, err
+	}
+	pieceHashes, err := bto.Info.splitPieceHashes()
+	if err != nil {
+		return TorrentFile{}, err
+	}
+	t := TorrentFile{
+		Announce:    bto.Announce,
+		InfoHash:    infoHash,
+		PieceHashes: pieceHashes,
+		PieceLength: bto.Info.PieceLength,
+		Length:      bto.Info.Length,
+		Name:        bto.Info.Name,
+	}
+	return t, nil
 }
